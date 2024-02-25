372600701a4be34b82ed450eaa286721831a49d0
gloryYam <dudrhkd3892@gmail.com>
2024-02-25 23:17:35 +0900

    tdd를 활용하여 주문생성, 상품 조회 구현

---
+++ src/test/java/sample/cafekiosk/spring/domain/product/repository/ProductRepositoryTest.java
@@ -0,0 +1,120 @@
+package sample.cafekiosk.spring.domain.product.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.junit.jupiter.api.Assertions.*;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
+import static sample.cafekiosk.spring.domain.product.ProductType.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    @DisplayName("원하는 판매상태를 가진 상품들을 조회")
+    void findAllBySellingStatusIn() {
+        // given
+        Product product1 = Product.builder()
+                .productNumber("001")
+                .type(HANDMADE)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(HANDMADE)
+                .sellingStatus(HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+
+        Product product3 = Product.builder()
+                .productNumber("003")
+                .type(HANDMADE)
+                .sellingStatus(STOP_SELLING)
+                .name("빙수")
+                .price(8000)
+                .build();
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        List<Product> products = productRepository.findAllBySellingStatusIn(List.of(SELLING, HOLD));
+
+        // then
+
+        /**
+         *  리스트를 검증할 때 처음에는 사이즈 체크 그리고
+         *  extracting, contains 를 주로 사용
+         */
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "sellingStatus")  // 검증하고자 하는 필드 추출 가능
+                .containsExactlyInAnyOrder(     // 순서 상관없이 (반대 -> containsExactly())
+                        tuple("001", "아메리카노", SELLING),
+                        tuple("002", "카페라떼", HOLD)
+                );
+    }
+
+    @Test
+    @DisplayName("상품번호 리스트로 상품들을 조회한다.")
+    void findAllByProductNumberIn() {
+        // given
+        Product product1 = Product.builder()
+                .productNumber("001")
+                .type(HANDMADE)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(HANDMADE)
+                .sellingStatus(HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+
+        Product product3 = Product.builder()
+                .productNumber("003")
+                .type(HANDMADE)
+                .sellingStatus(STOP_SELLING)
+                .name("빙수")
+                .price(8000)
+                .build();
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        List<Product> products = productRepository.findAllByProductNumberIn(List.of("001", "002"));
+
+        // then
+
+        /**
+         *  리스트를 검증할 때 처음에는 사이즈 체크 그리고
+         *  extracting, contains 를 주로 사용
+         */
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "sellingStatus")  // 검증하고자 하는 필드 추출 가능
+                .containsExactlyInAnyOrder(     // 순서 상관없이 (반대 -> containsExactly())
+                        tuple("001", "아메리카노", SELLING),
+                        tuple("002", "카페라떼", HOLD)
+                );
+    }
+}
